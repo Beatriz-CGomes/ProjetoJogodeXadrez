ec897b2fb61f0c037d6118cbce46bf0681304515
Beatriz-CGomes <Beatriz.cg15@hotmail.com>
2023-03-22 21:48:56 -0300

    Starting project chess game

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Scanner;
+
+import chess.ChessMatch;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+
+		System.out.println();
+
+		sc.close();
+	}
+
+}
